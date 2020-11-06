ba374ecca6db69b3cba77291afe997a8cf13533b
Clément van Staen <clement.vanstaen@gmail.com>
2020-11-06 15:18:36 +0100

    DElete errors thrown for nothing

--- api/auth.js
+++ api/auth.js
@@ -8,11 +8,9 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   if (!req.body.password) {
     return res.status(400).json({ error: `No password was provided` });
-    throw new Error("No password was provided!");
   }
   if (!req.body.email) {
     return res.status(400).json({ error: `No email was provided` });
-    throw new Error("No email was provided!");
   }
 
   const email = req.body.email;
@@ -21,14 +19,12 @@ router.post("/", async (req, res) => {
 
   if (!user) {
     return res.status(400).json({ error: `User does not exist!` });
-    throw new Error("User does not exist!");
   }
 
   const isValid = await bcrypt.compare(password, user.password);
 
   if (!isValid) {
     return res.status(400).json({ error: `Password is incorrect!` });
-    throw new Error("Password is incorrect!");
   }
 
   const token = await jsonwebtoken.sign(
