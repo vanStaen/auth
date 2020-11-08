8e28b00ab1632c9180bfb2b686a2c23485f87fcf
Clément van Staen <clement.vanstaen@gmail.com>
2020-11-08 20:40:38 +0100

    switch refresh tokens in db

--- api/token.js
+++ api/token.js
@@ -11,35 +11,49 @@ router.post("/", async (req, res) => {
   }
   const refreshToken = req.body.refreshToken;
   const token = await Token.findOne({ token: refreshToken });
-
   if (!token) {
     return res.status(401).json({ error: `Refresh Token does not exist!` });
   }
 
+  // Check token validity
   const isTokenValid = await jsonwebtoken.verify(
     refreshToken,
     process.env.AUTH_SECRET_KEY_REFRESH
   );
-
   if (!isTokenValid) {
     return res.status(400).json({ error: `Refresh Token is incorrect!` });
   }
 
+  // Generate new Tokens
   const accessToken = await jsonwebtoken.sign(
     { userId: isTokenValid.userId, email: isTokenValid.email },
     process.env.AUTH_SECRET_KEY,
     { expiresIn: "15m" }
   );
-
-  const refreshToken = await jsonwebtoken.sign(
+  const newRefreshToken = await jsonwebtoken.sign(
     { userId: isTokenValid.userId, email: isTokenValid.email },
     process.env.AUTH_SECRET_KEY_REFRESH,
     { expiresIn: "7d" }
   );
 
+  // Delete Old refresh token!
+  const deleteToken = await Token.deleteOne({ token: refreshToken });
+  if (deleteToken.deletedCount === 0) {
+    return res.status(401).json({ error: `Refresh token does not exist!` });
+  }
+
+  // Add new refresh token to db
+  const newToken = new Token({
+    token: newRefreshToken,
+    userId: isTokenValid.userId,
+    email: isTokenValid.email,
+  });
+  const savedToken = await newToken.save();
+
+  // Return new tokens
   res.json({
     token: accessToken,
-    refreshToken: refreshToken,
+    refreshToken: newRefreshToken,
   });
 });
 
