b6e8d17f7cd650d7333d4821217875b4ec141a3d
cvanstaen <clement.van.staen@zalando.de>
2021-03-26 20:36:21 +0100

    patch passport endpoint WIP

--- api/passport.js
+++ api/passport.js
@@ -50,21 +50,21 @@ router.post("/", async (req, res) => {
 // PATCH update Passport
 router.patch("/", async (req, res) => {
     Passport.findOne({ email: req.body.email })
-        .then((passport) => {
-            if (passport) {
-                res.status(400).json({ error: "There is already an account associated to this email." });
+        .then(async (passport) => {
+            if (!passport) {
+                res.status(400).json({ error: "There is no account associated to this email." });
             }
-            return bcrypt.hash(req.body.password, 12);
+            const isValid = await bcrypt.compare(req.body.oldpassword, passport.password);
+            if (!isValid) {
+                res.status(400).json({ error: "The old password do not match." });
+            }
+            return passport._id;
         })
-        .then((hashedPassword) => {
-            const passport = new Passport({
-                name: req.body.name,
-                email: req.body.email,
-                password: hashedPassword,
-                active: true,
-            });
-            passport.save();
-            res.status(201).json({ passport: passport });
+        .then((userId) => {
+            const hashedNewPassword = bcrypt.hash(req.body.newpassword, 12);
+            const updateObject = { password: hashedNewPassword };
+            Passport.update({ _id: ObjectId(userId) }, { $set: updateObject });
+            res.status(201).json({ message: "Password successfully updated!" });
         })
         .catch((err) => {
             res.status(400).json({
